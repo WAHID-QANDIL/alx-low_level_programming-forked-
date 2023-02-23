38276f7df7056978c4acd3f323dc0fd771202bce
pha12thom <geofreymilugo@gmail.com>
2023-02-23 07:55:47 -0800

    i get it now

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File - main.h
+ * Author: Genius villagers
+ * Desc: Header contains protypes for all functions
+ * 	used in 0x03-more_functions_nested_loops directory
+ */
+
+int _isupper(int c);
+
+
+
+
+
+#endif
